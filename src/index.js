058391a6e055a9b0bbb007a469e3a39ae6abc391
Eduardo Sant'Ana <edusantsales@gmail.com>
2021-04-10 17:39:28 -0300

    Implementado os middlewares do desafio.

--- src/index.js
+++ src/index.js
@@ -10,19 +10,54 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    return response.status(404)
+  }
+  request.user = user
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+  if (!user.pro && user.todos.length < 10) {
+    return next()
+  }
+  if (!user.pro && user.todos.length === 10) {
+    return response.status(403)
+  }
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    return response.status(404)
+  }
+  const isUuid = validate(id)
+  if (!isUuid) {
+    return response.status(400)
+  }
+  const todo = user.todos.find(todo => todo.id === id)
+  if (!todo) {
+    return response.status(404)
+  }
+  request.user = user
+  request.todo = todo
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+  const user = users.find(user => user.id === id)
+  if (!user) {
+    return response.status(404)
+  }
+  request.user = user
+  return next()
 }
 
 app.post('/users', (request, response) => {
